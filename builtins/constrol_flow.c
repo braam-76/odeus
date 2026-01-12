451435a6fd64e371fa00a7bd2d9955cdbc014a5f
braam-76 <braam67.gamidov@yandex.com>
2026-01-12 20:17:49 +0300

    Added 'if', 'and' and 'or' function

---
+++ builtins/constrol_flow.c
@@ -0,0 +1,78 @@
+#include "builtins/constrol_flow.h"
+#include "core/ast.h"
+#include "core/eval.h"
+
+AST *
+builtin_eq (AST *environment, AST *arguments)
+{
+  if (IS_NULL (arguments) || IS_NULL (CADR (arguments)) || !IS_NULL (CDDR (arguments)))
+    return make_error ("ERROR: eq expects two argument\n");
+
+  AST *first_argument = evaluate_expression (environment, CAR (arguments));
+  AST *second_argument = evaluate_expression (environment, CADR (arguments));
+
+  return first_argument == second_argument ? t () : nil ();
+}
+
+AST *
+builtin_null (AST *environment, AST *arguments)
+{
+  if (IS_NULL (arguments) || !IS_NULL (CDR (arguments)))
+    return make_error ("ERROR: null expects one argument\n");
+
+  return evaluate_expression (environment, CAR (arguments)) == nil () ? t () : nil ();
+}
+
+AST *
+builtin_if (AST *environment, AST *arguments)
+{
+  if (IS_NULL (arguments) || arguments_length (arguments) != 3)
+    return make_error ("ERROR: if expects 3 arguments: (if <cond> <then_clause> <else_clause>)\n");
+
+  AST *condition = evaluate_expression (environment, CAR (arguments));
+  AST *then_clause = CADR (arguments);
+  AST *else_clause = CADR (CDR (arguments));
+
+  if (IS_NULL (condition))
+    return evaluate_expression (environment, else_clause);
+  else
+    return evaluate_expression (environment, then_clause);
+}
+
+AST *
+builtin_and (AST *environment, AST *arguments)
+{
+  if (IS_NULL (arguments))
+    return make_error ("ERROR: and expects at least 1 argument\n");
+
+  while (arguments->type == AST_CONS)
+    {
+      AST *current_condition = evaluate_expression (environment, CAR (arguments));
+
+      if (IS_NULL (current_condition))
+        return nil ();
+
+      arguments = CDR (arguments);
+    }
+
+  return t ();
+}
+
+AST *
+builtin_or (AST *environment, AST *arguments)
+{
+  if (IS_NULL (arguments))
+    return make_error ("ERROR: or expects at least 1 argument\n");
+
+  while (arguments->type == AST_CONS)
+    {
+      AST *current_condition = evaluate_expression (environment, CAR (arguments));
+
+      if (!IS_NULL (current_condition))
+        return t ();
+
+      arguments = CDR (arguments);
+    }
+
+  return nil ();
+}
---
+++ builtins/include/builtins/constrol_flow.h
@@ -0,0 +1,14 @@
+#ifndef CONSTROL_FLOW_H_
+#define CONSTROL_FLOW_H_
+
+#include "core/ast.h"
+#include "core/eval.h"
+
+AST *builtin_eq (AST *environment, AST *arguments);
+AST *builtin_null (AST *environment, AST *arguments);
+AST* builtin_if(AST* environment, AST* arguments);
+AST* builtin_and(AST* environment, AST* arguments);
+AST* builtin_or(AST* environment, AST* arguments);
+
+
+#endif // CONSTROL_FLOW_H_
